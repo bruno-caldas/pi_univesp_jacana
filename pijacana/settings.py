39104735b027c01c45bab490994e26840f604874
Bruno Caldas <bruno.caldas@gmail.com>
2021-08-25 20:52:05 -0300

    preparando para deploy no heroku

--- pijacana/settings.py
+++ pijacana/settings.py
@@ -28,6 +28,12 @@ else:
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_TMP = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+
+os.makedirs(STATIC_TMP, exist_ok=True)
+os.makedirs(STATIC_ROOT, exist_ok=True)
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
