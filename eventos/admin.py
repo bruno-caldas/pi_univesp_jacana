20673cdaf6f8de7d4cdc29e50a5f80ca6b6d8ec0
VinicusGB <viniciusgomesb@gmail.com>
2021-09-25 11:21:45 -0300

    fix: Atualizações e correções
    Se aceito, este commit atualiza eventos.admin e corrige eventos.html

--- eventos/admin.py
+++ eventos/admin.py
@@ -6,6 +6,7 @@ from eventos.models import Evento
 class AdminEvento(admin.ModelAdmin):
     list_display = ('id','nome_evento','data_evento')
     list_filter = ('nome_evento','data_evento')
+    list_display_links = ('id','nome_evento','data_evento')
     ordering = ['data_evento','data_evento']
     search_fields = ['nome_evento']
 
