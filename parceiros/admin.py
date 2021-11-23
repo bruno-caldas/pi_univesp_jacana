a34f2e3225e4860c25504a408cae6cffdffa2404
VinicusGB <viniciusgomesb@gmail.com>
2021-11-23 20:02:55 -0300

    feat: alterações e melhorias em parceiros

--- parceiros/admin.py
+++ parceiros/admin.py
@@ -1,6 +1,14 @@
+from typing import ClassVar
 from django.contrib import admin
 
 from parceiros.models import Parceiros
 
 # Register your models here.
-admin.site.register(Parceiros)
+class AdminParceiro(admin.ModelAdmin):
+    list_display = ('id','nome_parceiro')
+    list_filter = ['nome_parceiro']
+    list_display_links = ('id','nome_parceiro')
+    ordering = ['nome_parceiro']
+    search_fields = ['nome_parceiro']
+
+admin.site.register(Parceiros,AdminParceiro)
--- parceiros/models.py
+++ parceiros/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.db.models import base
+from djrichtextfield.models import RichTextField
 
 # Create your models here.
 class Parceiros(models.Model):
     nome_parceiro = models.CharField(max_length=100,null=False,blank=False,unique=True,verbose_name='Parceiro',help_text='Escreve o nome do Parceiro.')
-    descricao_parceiro = models.TextField(null=False,blank=False,unique=True,verbose_name='Resumo sobre o Parceiros',help_text='Descreva um resumo sobre o ramo do parceiro')
-    site_parceiro = models.CharField(max_length=250,null=False,blank=False,unique=True,verbose_name='Site',help_text='Insira o site do parceiro')
+    descricao_parceiro = RichTextField(default='')
+    site_parceiro = models.URLField(null=True,max_length=200,verbose_name='Site',help_text='Insira o site do parceiro.')
     class Meta:
         db_table = 'parceiro'
         ordering = ['nome_parceiro']
