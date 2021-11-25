37197c052820bd02ee360c9731a60174140f6ffb
VinicusGB <viniciusgomesb@gmail.com>
2021-11-25 09:12:07 -0300

    fix: correções no models do mural_fofura

--- cadastro/models.py
+++ cadastro/models.py
@@ -22,7 +22,6 @@ class Question(models.Model):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
-
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
@@ -33,6 +32,11 @@ class Choice(models.Model):
 
 class EspecieAnimal(models.Model):
     especie = models.CharField(max_length=50,null=False,blank=False,unique=True,verbose_name='Espécie',help_text='Escreve a espécie do animal.')
+    class Meta:
+        db_table = 'especie' #nome definido da tabela no DB
+        ordering = ['especie']
+    def __str__(self): #correção para admin.py na aparecer como object
+        return self.especie
 
 class Document(models.Model):
     SEXO_ANIMAL = (
@@ -51,9 +55,7 @@ class Document(models.Model):
     # docfile = models.FileField(upload_to='documents/%Y/%m/%d')
     # docfile = models.FileField(upload_to='cadastro/media')
     docfile = models.FileField(upload_to='cadastro/media/%Y_%m_%d')
-    nome_cachorro = models.CharField(max_length=30)
-    porte_cachorro = models.CharField(max_length=30)
-    idade_cachorro = models.IntegerField(default=0)
+    nome_cachorro = models.CharField(max_length=30,verbose_name='Nome',help_text='Insira o nome do animal.')
     descricao = RichTextField(default='')
     sexo = models.CharField(null=True,max_length=1,choices=SEXO_ANIMAL, verbose_name='Sexo',help_text='Escolha o sexo do animal.')
     tipo = models.CharField(null=True,max_length=1,choices=TIPO_ANIMAL, verbose_name='Tipo',help_text='Escolha o tipo do animal.')
@@ -61,4 +63,9 @@ class Document(models.Model):
     data_nascimento = models.DateTimeField(null=True,verbose_name='Nascimento',help_text='Coloque a data aproximada de nascimento do animal.')
     especie = models.ForeignKey(EspecieAnimal,on_delete=models.CASCADE, blank=True, null=True, default=2)
     url = models.URLField(null=True,max_length=200)
+    class Meta:
+        db_table = 'mural_fofura' #nome definido da tabela no DB
+        ordering = ['nome_cachorro','data_nascimento','tipo','porte']
+    def __str__(self): #correção para admin.py na aparecer como object
+        return self.nome_cachorro
     # pip install boto3 django-storages
